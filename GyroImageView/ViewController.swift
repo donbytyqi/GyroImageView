2bea1ff8b8d3a5d8734a19c742eb8887ad7da04e
Don <donbytyqi@outlook.com>
2019-07-09 16:34:15 +0200

    Initial Commit

---
+++ GyroImageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GyroImageView
+//
+//  Created by Don Bytyqi on 7/9/19.
+//  Copyright © 2019 Don Bytyqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
